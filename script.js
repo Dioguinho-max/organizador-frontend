64e94b017d6127b8cc10bf7d83cd794b2217e892
Diogo <santos.vieira.diogo@escola.pr.gov.br>
2026-02-14 08:21:46 -0300

    Change API endpoint to production URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://organizador-backend-dqxr.onrender.com";
 
 async function register() {
     const username = document.getElementById("username").value;
